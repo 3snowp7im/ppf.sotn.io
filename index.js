c4db94ae831dc102ad65eaa9fe8f2eb6ba988754
Wild Mouse <3snowp7im@gmail.com>
2022-08-05 20:00:40 -0500

    Add sha1 checker option

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const elems = {
   ppf: document.getElementById('ppf'),
   bin: document.getElementById('bin'),
+  sha1: document.getElementById('sha1'),
   ppfFile: document.getElementById('ppf-file'),
   binFile: document.getElementById('bin-file'),
+  sha1File: document.getElementById('sha1-file'),
   ppfStatus: document.getElementById('ppf-status'),
   binStatus: document.getElementById('bin-status'),
+  sha1Status: document.getElementById('sha1-status'),
+  disc: document.getElementById('disc'),
   button: document.getElementById('apply-button'),
   download: document.getElementById('download'),
   notification: document.getElementById('notification'),
@@ -15,23 +19,34 @@ function dragOverListener(event) {
   event.preventDefault()
   event.stopPropagation()
   event.dataTransfer.dropEffect = 'copy'
-  event.target.classList.add('active')
+  {[elems.ppf, elems.bin, elems.sha1].forEach(function(elem) {
+    elem.classList.add('active')
+  })}
 }
 
 function dragLeaveListener(event) {
-  event.target.classList.remove('active')
+  removeActiveClasses()
 }
 
-const selectedFiles = [{}, {}]
+function removeActiveClasses() {
+  [elems.ppf, elems.bin, elems.sha1].forEach(function(elem) {
+    elem.classList.remove('active')
+  })
+}
+
+const selectedFiles = [{}, {}, {}]
 const fileMap = new Map()
 fileMap.set(elems.ppf, selectedFiles[0])
 fileMap.set(elems.bin, selectedFiles[1])
+fileMap.set(elems.sha1, selectedFiles[2])
 fileMap.set(elems.ppfFile, selectedFiles[0])
 fileMap.set(elems.binFile, selectedFiles[1])
+fileMap.set(elems.sha1File, selectedFiles[2])
 
 const statusMap = new Map()
 statusMap.set(elems.ppf, [elems.ppfStatus, elems.ppfFile])
 statusMap.set(elems.bin, [elems.binStatus, elems.binFile])
+statusMap.set(elems.sha1, [elems.sha1Status, elems.sha1File])
 
 function updateStatus() {
   if (selectedFiles[0].file && selectedFiles[1].file) {
@@ -50,43 +65,79 @@ function fileChange(event) {
 function dropListener(event) {
   event.preventDefault()
   event.stopPropagation()
-  let name
-  let target = event.target
-  if (!fileMap.get(event.target)) {
-    target = event.target.parentElement
-  }
+  removeActiveClasses()
+  let files
   if (event.dataTransfer.items) {
-    for (let i = 0; i < event.dataTransfer.items.length; i++) {
-      const item = event.dataTransfer.items[i]
-      if (item.kind === 'file') {
-        const file = item.getAsFile()
-        fileMap.get(target).file = file
-        name = file.name
+    files = Array.prototype.filter.call(
+      event.dataTransfer.items,
+      function(item) {
+        return item.kind === 'file'
       }
-    }
+    ).map(function(item) {
+      return item.getAsFile()
+    })
   } else {
-    for (let i = 0; i < event.dataTransfer.files.length; i++) {
-      const file = event.dataTransfer.files[i]
-      fileMap.get(target).file = file
-      name = file.name
-    }
+    files = event.dataTransfer.files
   }
-  const status = statusMap.get(target)
-  status[0].innerText = name
-  status[1].style.display = 'none'
-  event.target.classList.remove('active')
-  updateStatus()
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i]
+    let target
+    if (/.*\.[pP][pP][fF]$/.test(file.name)) {
+      target = elems.ppf
+    } else if (/.*\.[bB][iI][nN]$/.test(file.name)) {
+      target = elems.bin
+    } else if (/.*\.[sS][hH][aA]1$/.test(file.name)) {
+      target = elems.sha1
+    }
+    fileMap.get(target).file = file
+    if (target === elems.sha1) {
+      updateDiscSelection()
+    }
+    const status = statusMap.get(target)
+    status[0].innerText = file.name
+    status[1].style.display = 'none'
+    updateStatus()
+  }
 }
 
-{[elems.ppf, elems.bin].forEach(function(slot) {
-  slot.addEventListener('dragover', dragOverListener)
-  slot.addEventListener('dragleave', dragLeaveListener)
-  slot.addEventListener('drop', dropListener)
-})}
+document.body.addEventListener('dragover', dragOverListener)
+document.body.addEventListener('dragleave', dragLeaveListener)
+document.body.addEventListener('drop', dropListener)
 
-{[elems.ppfFile, elems.binFile].forEach(function(slot) {
+{[elems.ppfFile, elems.binFile, elems.sha1File].forEach(function(slot) {
   slot.addEventListener('change', fileChange)
 })}
+
+function updateDiscSelection() {
+  while (elems.disc.firstChild) {
+    parent.removeChild(elems.disc.firstChild)
+  }
+  const reader = new FileReader()
+  reader.addEventListener('load', function() {
+    const lines = this.result.split('\r\n')
+    lines.pop()
+    if (!lines.every(function(line) {
+      return /^[a-f0-9]{40} \*.*$/.test(line)
+    })) {
+      notify('Invalid SHA1 file')
+    } else {
+      lines.forEach(function(line) {
+        if (!/\.cue$/.test(line)) {
+          const digest = line.match(/^([a-f0-9]{40})/)[1]
+          const disc = line.match(/^[a-f0-9]{40} \*(.*)$/)[1]
+          const option = document.createElement('option')
+          option.value = digest
+          option.innerText = disc
+          elems.disc.appendChild(option)
+        }
+      })
+      elems.disc.classList.add('visible')
+    }
+  })
+  reader.readAsBinaryString(selectedFiles[2].file)
+}
+
+elems.sha1File.addEventListener('change', updateDiscSelection)
 
 function getUrl() {
   const url = new URL(window.location.href)
@@ -102,7 +153,7 @@ elems.button.addEventListener('click', function(event) {
   let filesRead = 0
   const files = new Array(2)
   const name = selectedFiles[1].file.name
-  selectedFiles.forEach(function(o, index) {
+  selectedFiles.slice(0, 2).forEach(function(o, index) {
     if (o.file) {
       const reader = new FileReader()
       reader.addEventListener('load', function() {
@@ -112,6 +163,7 @@ elems.button.addEventListener('click', function(event) {
             url: getUrl(),
             ppf: files[0],
             bin: files[1],
+            sha1: elems.disc.value,
             name: name,
           }, files)
         }
@@ -148,18 +200,22 @@ worker.addEventListener('message', function(message) {
     elems.download.click()
     URL.revokeObjectURL(url)
   } else if (message.error) {
-    if (animationDone) {
-      animationDone = false
-      elems.notificationText.innerText = message.error
-      elems.notification.classList.add('show')
-      elems.notification.classList.remove('hide')
-      setTimeout(function() {
-        elems.notification.classList.add('hide')
-      }, 5000)
-      setTimeout(function() {
-        elems.notification.classList.remove('show')
-        animationDone = true
-      }, 7000)
-    }
+    notify(message.error)
   }
 })
+
+function notify(message) {
+  if (animationDone) {
+    animationDone = false
+    elems.notificationText.innerText = message
+    elems.notification.classList.add('show')
+    elems.notification.classList.remove('hide')
+    setTimeout(function() {
+      elems.notification.classList.add('hide')
+    }, 5000)
+    setTimeout(function() {
+      elems.notification.classList.remove('show')
+      animationDone = true
+    }, 7000)
+  }
+}
--- worker.js
+++ worker.js
@@ -12,20 +12,51 @@ function ppfWorker() {
     eccEdcCalc = self.eccEdcCalc
   }
 
+  function sha1(input) {
+    return crypto.subtle.digest('SHA-1', input).then(function(buf) {
+      return new Uint8Array(buf)
+    })
+  }
+
+  function patch(ppf, bin, name) {
+    const ppfArray = new Uint8Array(ppf)
+    const binArray = new Uint8Array(bin)
+    try {
+      apply(ppfArray, binArray)
+      eccEdcCalc(binArray, binArray.length)
+      this.postMessage({
+        bin: bin,
+        name: name,
+      }, [bin])
+    } catch (err) {
+      this.postMessage({error: err.message})
+    }
+  }
+
   function handleMessage(message) {
     message = message.data
     loadBrowser(message.url)
-    try {
-      const ppf = new Uint8Array(message.ppf)
-      const bin = new Uint8Array(message.bin)
-      apply(ppf, bin)
-      eccEdcCalc(bin, bin.length)
-      this.postMessage({
-        bin: message.bin,
-        name: message.name,
-      }, [message.bin])
-    } catch (err) {
-      this.postMessage({error: err.message})
+    const ppf = message.ppf
+    const bin = message.bin
+    const name = message.name
+    const self = this
+    if (message.sha1) {
+      const expected = Uint8Array.from(
+        message.sha1.match(/.{2}/g).map(function(byte) {
+          return parseInt(byte, 16)
+        })
+      )
+      sha1(new Uint8Array(bin)).then(function(actual) {
+        for (let i = 0; i < 20; i++) {
+          if (actual[i] != expected[i]) {
+            self.postMessage({error: 'Invalid BIN file'})
+            return
+          }
+        }
+        patch.call(self, ppf, bin, name)
+      })
+    } else {
+      patch.call(self, ppf, bin, name)
     }
   }
 
